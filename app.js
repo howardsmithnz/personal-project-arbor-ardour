9a44854bd9af286428420656b858a13975882234
howardsmithnz <howardsmithnz@gmail.com>
2016-02-17 19:06:03 +1300

    basic html form and css setup

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  // res.send(template());
+  console.log("GET for / received")
+});
+
+app.post('/', function (req, res) {
+  res.json({ user: 'tobi' });
+  // TO DO: echo back the input values from the user as a JSON object
+  // res.send('Got a POST request');
+  console.log("POST for / received")
+});
+
+app.listen(3000, function () {
+  console.log('Tree Tracker app listening on port 3000!');
+});
