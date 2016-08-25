ed23a914deba3ed5bf5d2b648582100eae259552
Howard Smith <howardsmithnz@gmail.com>
2016-08-25 17:06:05 +1200

    tweak of test file

--- test/index.js
+++ test/index.js
@@ -2,18 +2,22 @@ var test = require('tape')
 var app = require('../index.js')
 
 // var sayBoo = 
-//arrange: fakeDate, expectedResults
+// arrange: fakeDate, expectedResults
 
-test('simple test', function(t) {
-	t.plan(3)
-	//action: call the function and assign to 'actualSmthng'
-	var boo = app.sayBoo()
+test('simple test', function (t) {
+  t.plan(3)
+  // action: call the function and assign to 'actualSmthng'
+  var boo = app.sayBoo()
 
-    //assert: test if actual equals expected
-    t.ok(1)
-    t.equal(1,1, "It's 1!")
-	t.equal(boo, "Boo!", "These both should say Boo!")
+  // assert: test if actual equals expected
+  t.ok(1, "I'm OK")
+  t.equal(1, 1, "It's 1!")
+  t.equal(boo, 'Boo!', 'These both should say Boo!')
 
-	t.end()
-	
+  t.end()
+// t.timeoutAfter(2000)
+})
+
+test.onFinish(function () {
+  console.log('All done')
 })
